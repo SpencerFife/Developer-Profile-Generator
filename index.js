e39ff1c9f291ebb57d030201e4236ba76b8684db
Spencer Fife <scottyfife17@gmail.com>
2020-02-28 14:05:20 -0800

    inquirer and axios constants and questions object

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 "use strict";
 
-const questions = [];
+const inquirer = require("inquirer");
+const axios = require("axios");
+
+const questions = [
+  {
+    type: "input",
+    name: "userName",
+    message: "What is your GitHub user name?"
+  },
+  {
+    type: "list",
+    name: "colors",
+    message: "What is your favorite color?",
+    choices: ["green", "blue", "red", "pink"]
+  }
+];
 
 function writeToFile(fileName, data) {}
 
