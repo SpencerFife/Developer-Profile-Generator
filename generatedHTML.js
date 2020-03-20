72207fecadd7647e04905896636da6e30ea63465
Spencer Fife <scottyfife17@gmail.com>
2020-03-20 14:43:58 -0700

    added to generatedHTML w/ template literals

--- generatedHTML.js
+++ generatedHTML.js
@@ -29,148 +29,98 @@ const colors = {
 
 function generateHTML(data) {
   return `<!DOCTYPE html>
-<html lang="en">
-   <head>
+  <html lang="en">
+    <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-      <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous"
+      />
       <title>Document</title>
       <style>
-          @page {
-            margin: 0;
-          }
-         *,
-         *::after,
-         *::before {
-         box-sizing: border-box;
-         }
-         html, body {
-         padding: 0;
-         margin: 0;
-         }
-         html, body, .wrapper {
-         height: 100%;
-         }
-         .wrapper {
-         background-color: ${colors[data.color].wrapperBackground};
-         padding-top: 100px;
-         }
-         body {
-         background-color: white;
-         -webkit-print-color-adjust: exact !important;
-         font-family: 'Cabin', sans-serif;
-         }
-         main {
-         background-color: #E9EDEE;
-         height: auto;
-         padding-top: 30px;
-         }
-         h1, h2, h3, h4, h5, h6 {
-         font-family: 'BioRhyme', serif;
-         margin: 0;
-         }
-         h1 {
-         font-size: 3em;
-         }
-         h2 {
-         font-size: 2.5em;
-         }
-         h3 {
-         font-size: 2em;
-         }
-         h4 {
-         font-size: 1.5em;
-         }
-         h5 {
-         font-size: 1.3em;
-         }
-         h6 {
-         font-size: 1.2em;
-         }
-         .photo-header {
-         position: relative;
-         margin: 0 auto;
-         margin-bottom: -50px;
-         display: flex;
-         justify-content: center;
-         flex-wrap: wrap;
-         background-color: ${colors[data.color].headerBackground};
-         color: ${colors[data.color].headerColor};
-         padding: 10px;
-         width: 95%;
-         border-radius: 6px;
-         }
-         .photo-header img {
-         width: 250px;
-         height: 250px;
-         border-radius: 50%;
-         object-fit: cover;
-         margin-top: -75px;
-         border: 6px solid ${colors[data.color].photoBorderColor};
-         box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-         }
-         .photo-header h1, .photo-header h2 {
-         width: 100%;
-         text-align: center;
-         }
-         .photo-header h1 {
-         margin-top: 10px;
-         }
-         .links-nav {
-         width: 100%;
-         text-align: center;
-         padding: 20px 0;
-         font-size: 1.1em;
-         }
-         .nav-link {
-         display: inline-block;
-         margin: 5px 10px;
-         }
-         .workExp-date {
-         font-style: italic;
-         font-size: .7em;
-         text-align: right;
-         margin-top: 10px;
-         }
-         .container {
-         padding: 50px;
-         padding-left: 100px;
-         padding-right: 100px;
-         }
-
-         .row {
-           display: flex;
-           flex-wrap: wrap;
-           justify-content: space-between;
-           margin-top: 20px;
-           margin-bottom: 20px;
-         }
-
-         .card {
-           padding: 20px;
-           border-radius: 6px;
-           background-color: ${colors[data.color].headerBackground};
-           color: ${colors[data.color].headerColor};
-           margin: 20px;
-         }
-         
-         .col {
-         flex: 1;
-         text-align: center;
-         }
-
-         a, a:hover {
-         text-decoration: none;
-         color: inherit;
-         font-weight: bold;
-         }
-
-         @media print { 
-          body { 
-            zoom: .75; 
-          } 
-         }
-      </style>`;
+        a {
+          color: ${colors[data.color].headerColor};
+        }
+        .card,
+        .card-body,
+        .jumbotron {
+          background-color: ${colors[data.color].headerColor};
+          color: ${colors[data.color].headerColor};
+        }
+        img {
+          border: 6px solid ${colors[data.color].photoBorderColor};
+        }
+        body {
+          background-color: ${colors[data.color].wrapperBackground};
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <div class="jumbotron text-center">
+          <img
+            src="${data.avatar_url}"
+            alt="..."
+            class="rounded-circle w-50"
+          />
+          <h1 class="display-4">Hi!</h1>
+          <h2 class="lead">
+            My name is ${$data.name}!
+          </h2>
+          <hr class="my-4" />
+          <h5>Currently @ ${data.company}</h5>
+          <a href="https://www.google.com/maps/place/" + ${data.location}>${
+    data.location
+  }</a>
+          <a href="${data.html_url}">GitHub</a>
+          <a href="${data.blog}">Blog</a>
+        </div>
+        <div class="row">
+          <div class="col-sm-6 my-3">
+            <div class="card">
+              <div class="card-body text-center">
+                <h5 class="card-title">Public Repositories</h5>
+                <p class="card-text">
+                  ${data.public_repos}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 my-3">
+            <div class="card">
+              <div class="card-body text-center">
+                <h5 class="card-title">Followers</h5>
+                <p class="card-text">
+                  ${data.followers}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 my-3">
+            <div class="card">
+              <div class="card-body text-center">
+                <h5 class="card-title">GitHub Stars</h5>
+                <p class="card-text">
+                  ${data.star_numbers}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 my-3">
+            <div class="card">
+              <div class="card-body text-center">
+                <h5 class="card-title">Following</h5>
+                <p class="card-text">
+                  ${data.following}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  </html>`;
 }
